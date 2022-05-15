711a43c269328ed29af23c0ea0952003d40894ae
Evgeni <eugenluzin@gmail.com>
2022-05-15 20:20:12 +0500

    Вывод времени в таблицу. Завершение!

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -11,7 +11,7 @@ List<int> CountWorkers = new List<int>()
     10000,
     100000,
 };
-List<int> CountThreads = new List<int>() {1,2,4,8,16};
+List<int> CountThreads = new List<int>() {1,2,4,8};
 Console.WriteLine(0);
 List<List<Worker>> WorkersList = new List<List<Worker>>();
 List<List<double>> time = new List<List<double>>();
@@ -19,46 +19,108 @@ List<List<double>> time = new List<List<double>>();
 var reqHelper = new RequestHelper();
 var ParallelReqHelepr = new ParallelRequestHelper(1);
 
-Parallel.For(0, CountWorkers.Count, i =>
+for(int i = 0; i<CountWorkers.Count; i++)
 {
     WorkersList.Add(WorkerHelper.GenerateWorkersList(CountWorkers[i]));
     Console.WriteLine(1 + i);
-});
+}
 Console.WriteLine(5);
 Stopwatch sch = new Stopwatch();
 IRequests req;
 for (int i = 0; i < CountThreads.Count; i++)
 {
     int countThread = CountThreads[i];
-    req = reqHelper;
+    /*req = reqHelper;
     if (countThread != 1)
     {
         ParallelReqHelepr.NumberThreads = countThread;
         req = ParallelReqHelepr;
-    }
-    
-    List<double> localTime = new();
-    for (int j = 0; j < WorkersList.Count; i++)
-    {
+    }*/
+    ParallelReqHelepr.NumberThreads = countThread;
+    req = ParallelReqHelepr;
 
+    List<double> localTime = new();
+    for (int j = 0; j < WorkersList.Count; j++)
+    {
+        string nameFunc = "";
         double lt = 0.0;
+        nameFunc = "SW  | ";
         for(int k = 0; k < 5; k++)
         {
+
+            sch.Restart();
             sch.Start();
-            var a = req.GetOrdersWhereFullNameStartsWith(WorkersList[i], 'a');
+            req.GetOrdersWhereFullNameStartsWith(WorkersList[j], 'a');
             sch.Stop();
             lt += sch.ElapsedMilliseconds / 1000.0;
-            //tm.Add(sch.ElapsedMilliseconds / 1000.0);
+            Console.WriteLine($"{nameFunc,6}{countThread,-2} | {WorkersList[j].Count,-7} | Итерация - {k,-2} | Время - {sch.ElapsedMilliseconds / 1000.0,-5}");
+
         }
         lt /= 5.0;
-        localTime.Add(lt);
-       
+        localTime.Add(Math.Round(lt,3));
+        //localTime.Add(nameFunc +$"{countThread,-2} | {WorkersList[j].Count,-7}",lt);
+
+        nameFunc = "BoAD| ";
+        lt = 0.0;
+        for (int k = 0; k < 5; k++)
+        {
+            sch.Restart();
+            sch.Start();
+            req.GetOrdersBeforeOrAfterDate(WorkersList[j], RequestHelper.BeforeOrAfter.After, new DateTime(2001, 01, 15));
+            sch.Stop();
+            lt += sch.ElapsedMilliseconds / 1000.0;
+            Console.WriteLine($"{nameFunc,6}{countThread,-2} | {WorkersList[j].Count,-7} | Итерация - {k,-2} | Время - {sch.ElapsedMilliseconds / 1000.0,-5}");
+
+        }
+        lt /= 5.0;
+        localTime.Add(Math.Round(lt, 3));
+        //localTime.Add(nameFunc + $"{countThread,-2} | {WorkersList[j].Count,-7}", lt);
+
+        nameFunc = "AVG | ";
+        lt = 0.0;
+        for (int k = 0; k < 5; k++)
+        {
+            sch.Restart();
+            sch.Start();
+            req.GetWorkersSortedByAVGOrderPrice(WorkersList[j]);
+            sch.Stop();
+            lt += sch.ElapsedMilliseconds / 1000.0;
+            Console.WriteLine($"{nameFunc,6}{countThread,-2} | {WorkersList[j].Count,-7} | Итерация - {k,-2} | Время - {sch.ElapsedMilliseconds / 1000.0,-5}");
+
+        }
+        lt /= 5.0;
+        //localTime.Add(nameFunc + $"{countThread,-2} | {WorkersList[j].Count,-7}", lt);
+        localTime.Add(Math.Round(lt,3));
     }
+    
     time.Add(localTime);
     
 
 }
+List<string> names = new List<string>()
+{
+    "| SW      |",
+    "| BoAD    |",
+    "| AVG     |"
+};
+for (int i = 0; i < WorkersList.Count; i++)
+{
+    string st = "|"+new string(' ', 9)+"|";
+    for (int j = 0; j < CountThreads.Count; j++)
+    {
+            st += $"{CountThreads[j],2}-{CountWorkers[i],-6}|";
+    }
+    Console.WriteLine(st);
+    
+    for (int j = 0; j < 3; j++)
+    {
+        string sst = names[j];
+        for (int k = 0; k < CountThreads.Count; k++)
+        {
+            sst += $"{time[k][j+3*i].ToString(),9}|";
+        }
+        Console.WriteLine(sst);
+    }
+    Console.WriteLine();
+}
 
-//var selectedWorkers = RequestHelper.GetWorkersSortedByAVGOrderPrice();
-//var selectedWorkers = RequestHelper.GetOrdersWhereFullNameStartsWith(a,'a');
-//var selectedWorkers= RequestHelper.GetOrdersBeforeOrAfterDate(a,RequestHelper.BeforeOrAfter.After,new DateTime(2001,01,15));
